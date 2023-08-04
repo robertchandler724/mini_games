02f48b255701357f103748d0dcf086d840471ad5
Robert <robert.chandler724@gmail.com>
2023-08-04 14:36:38 -0400

    madlib game added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mini_games
-a repo of mini games for python
+This is a repo of mini games written in Python
+
+1. madlibs
+    To run madlids, open CLI and run python3 madlibs.py. You will be prompted to fill in the words. Once complete, it will generate your madlib.
---
+++ madlibs.py
@@ -0,0 +1,25 @@
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+verb3 = input('Verb: ')
+verb4 = input('Verb: ')
+verb5 = input('Verb: ')
+verb6 = input('Verb: ')
+verb7 = input('Verb: ')
+noun1 = input('Noun: ')
+noun2 = input('Noun: ')
+plural_noun1 = input('Plural noun: ')
+plural_noun2 = input('Plural noun: ')
+color_plural1 = input('Color plural: ')
+color_plural2 = input('Color plural: ')
+number1 = input('Number: ')
+number2 = input('Number: ')
+
+madlib = f"In order to do your laundry, first you have to {verb1} your clothes. Then you have {verb2} the {noun1} \
+machine. Take your time not to {verb3} any items that may be gross. Separate your {plural_noun1} from your {plural_noun2} \
+and then load the {noun2} machine. Make sure not to mix the {color_plural1} with the {color_plural2}. \
+Set temperature to {number1} degrees and begin the cycle. After that is finished, {verb4} your clothes and {verb5} \
+them in the dryer. Set the temperature and {verb6} the dryer. After about {number2} minutes, it should be \
+complete! Now all you have to do is {verb7} your clothes and you are all set! {adj} laundry!"
+
+print(madlib)
